c70ade8b94a9c5b1c4d72410a76bf271799193e3
Sulatn Roziyev <jjo152065@gmail.com>
2023-04-16 16:25:27 +0600

    #3 Добавил представления регистрации аутентификации и выхода из аккаунта

--- HH/accounts/views.py
+++ HH/accounts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
+from accounts.forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('/')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('/')
+        login(request, user)
+        next = request.GET.get('next')
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        return redirect('/')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
