d6eec8f7db5199e37c7de6cde2a0b695346ce405
Гульняшкин Евгений <ducat.ea@gmail.com>
2023-04-18 18:03:34 +0600

    #7 добавлена модель Вакансии и вью для главной страницы

--- HH/headhunter/views.py
+++ HH/headhunter/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from urllib.parse import urlencode
+from django.db.models import Q
+from django.views.generic import ListView
+from .models import Vacancy
+
 
 # Create your views here.
+class IndexView(ListView):
+    template_name = 'index.html'
+    paginate_by = 20
+    model = Vacancy
+    context_object_name = 'vacancy'
+    
